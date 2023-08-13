47827d3c36eca18bb2a51d2e5e7ce7253cf40dd7
SongIsak <thddltkr10@naver.com>
2023-08-14 01:13:31 +0900

    url수정

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -275,9 +275,9 @@ export default {
           info: this.newBoard.info
         };
         if (this.newBoard.id) {
-          await axios.put("/boards/" + this.newBoard.id + "/names", boardData);
-          await axios.put("/boards/" + this.newBoard.id + "/colors", boardData);
-          await axios.put("/boards/" + this.newBoard.id + "/infos", boardData);
+          await axios.put("/boards/names/" + this.newBoard.id, boardData);
+          await axios.put("/boards/colors/" + this.newBoard.id, boardData);
+          await axios.put("/boards/infos/" + this.newBoard.id, boardData);
         } else {
           await axios.post("/boards", this.newBoard);
         }
